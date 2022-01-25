dff0c63fcd3456cf7e72c6d704cf1e15ffc6e365
Yuval <yuvalshai95@gmail.com>
2022-01-25 22:38:23 +0200

    mongo atlas url fix

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   dbURL:
-    'mongodb+srv://yuval-shai:<password>@skello-proj.3ysvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'mongodb+srv://yuval-shai:yuval-skello-proj@skello-proj.3ysvn.mongodb.net/skelloDB?retryWrites=true&w=majority',
 };
