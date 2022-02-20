c00fdd3df908bd3adf8abac66d66857ca29a29ce
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2022-02-20 16:49:08 +0200

    add atlas link

--- config/dev.js
+++ config/dev.js
@@ -2,4 +2,6 @@ module.exports = {
   dbURL:
     'mongodb+srv://yuval-shai:yuval-skello-proj@skello-proj.3ysvn.mongodb.net/skelloDB?retryWrites=true&w=majority',
   // 'dbURL': 'mongodb://localhost:27017',
+  // 'mongodb+srv://danielSkello:Ds09080911Ds@skello-projectca.gym30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 };
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
   dbURL:
     'mongodb+srv://yuval-shai:yuval-skello-proj@skello-proj.3ysvn.mongodb.net/skelloDB?retryWrites=true&w=majority',
-};
+    // 'mongodb+srv://danielSkello:Ds09080911Ds@skello-projectca.gym30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  };
