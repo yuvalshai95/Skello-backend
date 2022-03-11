4ce19989bd0b23932e69d98a078ecc67d3b06dfd
Yuval <yuvalshai95@gmail.com>
2022-03-11 11:51:54 +0200

    *git ignore

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,6 @@
+const {dbPassword} = require('../services/privateKeys.service');
 module.exports = {
-  dbURL:
-    'mongodb+srv://yuval-shai:yuval-skello-proj@skello-proj.3ysvn.mongodb.net/skelloDB?retryWrites=true&w=majority',
+  dbURL: `mongodb+srv://yuval-shai:${dbPassword}@skello-proj.3ysvn.mongodb.net/skelloDB?retryWrites=true&w=majority`,
   // 'dbURL': 'mongodb://localhost:27017',
   // 'mongodb+srv://danielSkello:Ds09080911Ds@skello-projectca.gym30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
 };
---
+++ services/privateKeys.service.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dbPassword: 'yuval-skello-proj',
+};
